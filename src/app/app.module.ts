aa023b675fbc636972f0408bf51928a5c0164247
DobriKaramihov <38349626+DobriKaramihov@users.noreply.github.com>
2018-06-06 20:27:39 +0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Log/login/login.component';
+import { RegisterComponent } from './Log/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
